2cd239e4e742a24df1ce0fb7b8217c34cfab6cf0
Muhammad Sadiq <mdsadiq@users.noreply.github.com>
2020-07-27 18:55:50 +0530

    initial commit

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,24 +1,41 @@
 import React, { useState } from "react";
 import Game from "./game";
 import { MODES } from "./constants";
+import { mobileCheck } from "./utils/utils";
 
 function Wrapper() {
-  const [mode, setMode] = useState(MODES.game);
+  const [mode, setMode] = useState(MODES.dev);
   const toggleMode = () => {
     mode === MODES.dev ? setMode(MODES.game) : setMode(MODES.dev);
   };
+
   return (
     <div tabIndex="0" className="App" id="app">
+      {mobileCheck() && (
+        <div className="mobile-error">
+          Cannot play on mobile.
+          <br />
+          Try opening it in system.
+          <br />
+        </div>
+      )}
+      {/* header */}
       <div className="header-info">
-        <h2>Feed the Snake</h2>
+        <h2>Feed the Snake[wip]</h2>
+        <h6>mdsadiq</h6>
       </div>
-
-      <Game toggleMode={toggleMode} mode={mode} />
+      {/* game */}
+      <div className="game-container">
+        <Game toggleMode={toggleMode} mode={mode} />
+      </div>
+      {/* footer */}
       <div className="footer-info">
         <div>
-          Mode: <span className="mode">{mode}</span>
+          Mode:
+          <button className="mode" onClick={toggleMode}>
+            {mode}
+          </button>
         </div>
-        <div />
       </div>
     </div>
   );
