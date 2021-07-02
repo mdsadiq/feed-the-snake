06b908c5f691eaab32e2dfb190269b9ae081d4a8
mdsadiq <mdsadiq6692@gmail.com>
2021-07-02 23:56:23 +0530

    upgrade react and code clean up

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
-import { Provider as ReduxProvider } from "react-redux";
-import reducers from "./reducers";
 import Wrapper from "./wrapper";
 import "./styles.css";
-const store = createStore(reducers);
+import reportWebVitals from "./reportWebVitals";
 
 function App() {
   return (
-    <ReduxProvider store={store}>
+    <StrictMode>
       <Wrapper />
-    </ReduxProvider>
+    </StrictMode>
   );
 }
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
